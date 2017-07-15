8bf27c3db9d9c2695bf8f1bd79149649249bebca
Alexander Demin <bouvens@gmail.com>
2017-07-15 22:08:01 +0600

    All available controls added to demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,90 +2,127 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import _ from 'lodash'
 
-import { Check, Connector, Input, SettersBlock } from '../../src'
+import { Check, Connector, Input, Radio, SettersBlock } from '../../src'
+import './style.css'
 
-const IDS = {
-    number: 'Number',
+const LABELS_FOR_ITERATIONS = {
+    number: 'N',
     plus: '+',
     multiplyTo: '×',
 }
 
+const IDS = {
+    divider: 'divider',
+    decimalMark: 'decimalMark',
+}
+
+const LABELS = {
+    [IDS.divider]: '÷',
+    [IDS.decimalMark]: 'Decimal mark for inputs and result'
+}
+
+const DIVIDERS = [2, 4, 8, 10]
+
+const DECIMAL_MARKS = ['.', ',', '·']
+
 const PRESETS = [
     {
         params: {
-            number: 0,
-            plus: 0,
-            multiplyTo: 0,
+            withDefault: true,
+            number: 2,
+            plus: 3.6,
+            multiplyTo: 4,
+            [IDS.divider]: DIVIDERS[0],
+            [IDS.decimalMark]: DECIMAL_MARKS[0],
         },
-        text: 'All zeros',
+        text: 'Default',
     },
     {
         params: {
             number: 1,
             plus: 2,
             multiplyTo: 3,
+            [IDS.divider]: DIVIDERS[1],
         },
-        text: '1, 2, 3',
+        text: '1, 2, 3, 4',
     },
     {
         params: {
             number: 100,
             plus: 25,
             multiplyTo: 2,
+            [IDS.divider]: DIVIDERS[3],
         },
         text: 'Other numbers',
     },
 ]
 
 class Demo extends Component {
-    state = {
-        withDefault: true,
-        number: 2,
-        plus: 3,
-        multiplyTo: 4,
-    }
+    state = PRESETS[0].params
 
     handleSave = (name, value) => {
         this.setState({ [name]: value })
     }
 
-    getCalculated = () => this.state.number + this.state.plus * this.state.multiplyTo
+    formatNumber = (number) => number.toString().replace('.', this.state.decimalMark)
+
+    getCalculated = () => this.formatNumber(
+        (this.state.number + this.state.plus * this.state.multiplyTo) / this.state.divider
+    )
 
     render () {
         return (
-            <div>
-                <h1>{'state-control Demo'}</h1>
+            <div className=".example">
+                <h1>{'StateControl Demo'}</h1>
+                <h2>Some presets</h2>
                 <SettersBlock
                     setters={PRESETS}
                     setHandler={this.handleSave}
                 />
+                <h2>State control</h2>
                 <Connector
                     state={this.state}
                     onChange={this.handleSave}
                     defaultNum={this.state.withDefault ? 1 : null}
+                    decimalMark={this.state.decimalMark}
                 >
                     <Check
                         id="withDefault"
-                        label="With default number"
+                        label="Inputs with default numbers"
                     />
-                    {_.reduce(IDS, (result, label, id) => result.concat(
+                    {_.reduce(LABELS_FOR_ITERATIONS, (result, label, id) => result.concat(
                         <Input
                             key={id}
                             id={id}
                             label={label}
                         />
                     ), [])}
+                    {LABELS.divider}
+                    <Radio
+                        id={IDS.divider}
+                        values={DIVIDERS}
+                    />
+                    {LABELS.decimalMark}
+                    <Radio
+                        id={IDS.decimalMark}
+                        values={DECIMAL_MARKS}
+                    />
                 </Connector>
+                <h2>Result</h2>
                 <p>
-                    {`${this.state.number} ${IDS.plus} ${this.state.plus} ${IDS.multiplyTo} ${this.state.multiplyTo}
-                     = ${this.getCalculated()}`}
+                    {`(${this.formatNumber(this.state.number)} ${LABELS_FOR_ITERATIONS.plus}
+                     ${this.formatNumber(this.state.plus)} ${LABELS_FOR_ITERATIONS.multiplyTo}
+                      ${this.formatNumber(this.state.multiplyTo)}) ${LABELS.divider}
+                       ${this.formatNumber(this.state.divider)} = ${this.getCalculated()}`}
                 </p>
                 <p>
-                    {this.state.withDefault
-                        ? 'With default numbers'
-                        : 'Without default numbers'
-                    }
+                    {this.state.withDefault ? 'Inputs with default numbers' : 'Inputs without default numbers'}
                 </p>
+                <h2>Source code and documentation</h2>
+                <ul>
+                    <li><a href="https://github.com/bouvens/state-control">Github</a></li>
+                    <li><a href="https://www.npmjs.com/package/state-control">npm</a></li>
+                </ul>
             </div>
         )
     }
