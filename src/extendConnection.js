73e8e304b5cb331871e2246d26800a511f7bfd51
Alexander Demin <bouvens@gmail.com>
2018-12-01 10:51:33 +0600

    refactor: Combine helpers

--- src/extendConnection.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash'
-
-const extendConnection = (props, IDS) => (state) => _.extend(props(state),
-  _.mapValues(IDS, (id) => _.get(state, id)))
-
-export default extendConnection
--- src/mapStateToIds.js
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash'
-
-const mapStateToIds = (state, IDS) => _.mapValues(_.invert(IDS), (id) => state[id])
-
-export default mapStateToIds
